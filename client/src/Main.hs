83b572c320b7c082a9ddd31868169f7489075d1f
Sönke Hahn <soenkehahn@gmail.com>
2016-06-14 20:36:25 +0800

    Add TreeVector state to client (not used yet)

--- client/src/Main.hs
+++ client/src/Main.hs
@@ -1,18 +1,23 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 
+import           Data.String
 import           React.Flux
+
+import           Api
 
 main :: IO ()
 main = reactRender "main" viewPatches ()
 
-store :: ReactStore ()
-store = mkStore ()
+store :: ReactStore Document
+store = mkStore mempty
 
-instance StoreData () where
-  type StoreAction () = ()
-  transform () () = return ()
+instance StoreData Document where
+  type StoreAction Document = ()
+  transform () doc = return doc
 
 viewPatches :: ReactView ()
-viewPatches = defineControllerView "patches app" store $ \ () () ->
-  text_ "huhu"
+viewPatches = defineControllerView "patches app" store $ \ doc () ->
+  text_ (fromString (show doc))

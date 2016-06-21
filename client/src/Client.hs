6621741c1062d01bf45729f7e7b680dafe61eb69
Sönke Hahn <soenkehahn@gmail.com>
2016-06-21 12:16:27 +0800

    add cursor to chat messages

--- client/src/Client.hs
+++ client/src/Client.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 
@@ -11,15 +12,15 @@ module Client where
 
 import           Control.Concurrent
 import           Control.DeepSeq
+import           Control.Monad
 import           Control.Monad.Trans.Except
 import           Data.Crdt.TreeVector
 import           Data.Crdt.TreeVector.Internal
-import           Data.Foldable
 import           Data.Functor
 import           Data.Monoid
 import           Data.String
 import           Data.String.Conversions
-import           Data.Text
+import           Data.Text (Text)
 import           GHC.Generics
 import           Network.HTTP.Client
 import           React.Flux
@@ -35,29 +36,43 @@ sync :<|> _ = client api
 
 run :: IO ()
 run = do
-  alterStore store Sync
+  alterStore store (Server Sync)
   reactRender "main" viewPatches ()
 
 data Model
   = Model {
     document :: Document,
-    inputField :: Text
+    cursor :: Int
   }
   deriving (Eq, Show)
 
 store :: ReactStore Model
-store = mkStore (Model mempty "")
+store = mkStore initial
+
+initial :: Model
+initial = Model mempty 0
 
 data Msg
-  = Update Document
-  | Sync
-  | Input Text
-  | Enter
+  = Server Server
+  | Ui Ui
 
   | Debug String
-  deriving (Generic)
+  deriving (Generic, Show)
+
+data Server
+  = Update Document
+  | Sync
+  deriving (Generic, Show)
+
+data Ui
+  = Enter Text
+  | UpArrow
+  | DownArrow
+  deriving (Generic, Show)
 
 instance NFData Msg
+instance NFData Ui
+instance NFData Server
 instance NFData a => NFData (TreeVector a)
 instance NFData a => NFData (Node a)
 instance NFData a => NFData (Element a)
@@ -66,57 +81,82 @@ instance NFData Data.Crdt.TreeVector.Client
 instance StoreData Model where
   type StoreAction Model = Msg
   transform = \ case
-    Sync -> \ model@(Model doc _) -> do
-      _ <- forkIO $ do
-        baseUrl <- sameOriginBaseUrl Nothing
-        result <- runExceptT $
-          sync doc (error "manager shouldn't be touched") baseUrl
-        _ <- forkIO (threadDelay 1000000 >> alterStore store Sync)
-        case result of
-          Right new ->
-            alterStore store $ Update new
-      return model
-
-    Update new -> \ (Model doc inputField) -> do
-      return $ Model (doc <> new) inputField
-
-    Input s -> \ (Model doc _) ->
-      return $ Model doc s
-    Enter -> \ (Model old new) ->
-      return $ Model (old <> mkPatch (Client 0) old (getVector old ++ [new])) ""
-
+    Server msg -> transformServer msg
+    Ui msg -> transformUi msg
     Debug msg -> \ model -> putStrLn msg $> model
+
+transformServer :: Server -> Model -> IO Model
+transformServer = \ case
+  Sync -> \ model@(Model doc _) -> do
+    _ <- forkIO $ do
+      putStrLn "syncing..."
+      baseUrl <- sameOriginBaseUrl Nothing
+      result <- runExceptT $
+        sync doc (error "manager shouldn't be touched") baseUrl
+      putStrLn "received sync data..."
+      _ <- forkIO (threadDelay 10000000 >> alterStore store (Server Sync))
+      case result of
+        Right new ->
+          alterStore store $ Server $ Update new
+    return model
+
+  Update new -> \ (Model doc cursor) -> do
+    return $ Model (doc <> new) cursor
+
+transformUi :: Ui -> Model -> IO Model
+transformUi = \ case
+  Enter new -> \ (Model old cursor) -> do
+    return $ Model
+      (old <> mkPatch (Client 0) old (getVector old ++ [new]))
+      cursor
+  UpArrow -> \ (Model doc cursor) ->
+    return $ Model doc (max 0 (cursor - 1))
+  DownArrow -> \ (Model doc cursor) ->
+    return $ Model doc (min (length $ getVector doc) (cursor + 1))
 
 viewPatches :: ReactView ()
 viewPatches = defineControllerView "patches app" store $ \ model () -> do
-  ul_ [] $ do
-    forM_ (getVector (document model)) $ \ message -> do
-      li_ [] $ do
-        text_ $ fromString $ cs message
-  view chatInput () mempty
-  br_ []
-  text_ $ fromString $ show (getVector $ document model)
-  br_ []
-  text_ $ fromString $ show model
+  div_ [arrowEvents] $ do
+
+    let elements = zip (Nothing : map Just (getVector (document model))) [0 ..]
+    forM_ elements $ \ (message, index) -> do
+      forM_ message $ \ m -> do
+        text_ $ fromString $ cs m
+        br_ []
+      when (index == cursor model) $ do
+        hr_ []
+
+    view chatInput () mempty
+    br_ []
+    text_ $ fromString $ show (getVector $ document model)
+    br_ []
+    text_ $ fromString $ show model
+    br_ []
 
 chatInput :: ReactView ()
-chatInput = defineControllerView "chat input" store $ \ model () -> do
+chatInput = defineStatefulView "chat input" "" $ \ (text :: Text) () -> do
   input_ $
-    ("value" &= inputField model) :
-    (onInput $ \ event ->
-       SomeStoreAction store (Input (target event "value")) :
-       SomeStoreAction store (Debug (show event)) :
-       []) :
-    (onEnter $ \ _ _ -> [SomeStoreAction store Enter]) :
+    ("value" &= text) :
+    (onInput $ \ event _ -> ([], Just $ target event "value")) :
+    (onEnter $ \ _ _ text -> ([SomeStoreAction store (Ui $ Enter text)], Just "")) :
     []
 
 -- * view utils
 
-onEnter :: (Event -> KeyboardEvent -> [SomeStoreAction]) -> PropertyOrHandler [SomeStoreAction]
+onEnter :: Monoid handler =>
+  (Event -> KeyboardEvent -> handler) -> PropertyOrHandler handler
 onEnter action = onKeyDown $ \ event keyboardEvent ->
   case keyCode keyboardEvent of
     13 -> action event keyboardEvent
-    _ -> []
+    _ -> mempty
 
 textfield_ :: [PropertyOrHandler eventHandler] -> ReactElementM eventHandler ()
 textfield_ props = textarea_ props (return ())
+
+arrowEvents :: PropertyOrHandler [SomeStoreAction]
+arrowEvents = onKeyDown $ \ _ keyboardEvent ->
+  maybe [] (\ msg -> [SomeStoreAction store (Ui msg)]) $
+  case keyCode keyboardEvent of
+   38 -> Just UpArrow
+   40 -> Just DownArrow
+   _ -> Nothing
--- client/test/ClientSpec.hs
+++ client/test/ClientSpec.hs
@@ -1,26 +1,89 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module ClientSpec where
 
+import           Control.Monad
 import           Data.Crdt.TreeVector
-import           React.Flux
+import           Data.List
+import           Data.Text (Text)
+import           React.Flux (transform)
 import           Test.Hspec
+import           Test.Hspec.QuickCheck
+import           Test.QuickCheck
+import           Test.QuickCheck.Instances ()
 
+import           Api
 import           Client
 
 spec :: Spec
 spec = do
-  describe "transform" $ do
+  describe "transformServer" $ do
     context "Update" $ do
       it "incorporates updates" $ do
         let model :: Model
-            model = Model mempty ""
+            model = Model mempty 0
             update = mkPatch (Client 0) mempty ["foo"]
-        transform (Update update) model `shouldReturn` Model update ""
+        transformServer (Update update) model `shouldReturn` Model update 0
 
+  describe "transformUi" $ modifyMaxSize (min 10) $ do
     context "Input" $ do
       it "changes the model" $ do
         let model :: Model
-            model = Model mempty ""
-        Model _ text <- transform (Input "foo") model
-        text `shouldBe` "foo"
+            model = Model mempty 0
+        Model doc _ <- transformUi (Enter "foo") model
+        getVector doc `shouldBe` ["foo"]
+
+    context "the cursor" $ do
+      it "increases + 1 through DownArrow" $ do
+        let doc = mkPatch (Client 0) mempty ["foo"]
+        transformUi DownArrow (Model doc 0) `shouldReturn` Model doc 1
+
+      it "decreases - 1 through UpArrow" $ do
+        transformUi UpArrow (Model mempty 1) `shouldReturn` Model mempty 0
+
+      context "is never out of bounds" $ do
+        it "is never too big" $ do
+          forAllModels $ \ (Model _ cursor) -> do
+            cursor >= 0
+
+        it "is never too small" $ do
+          forAllModels $ \ (Model doc cursor) -> do
+            cursor <= (length (getVector doc))
+
+forAllModels :: Testable t => (Model -> t) -> Property
+forAllModels cont = property $ \ (messages :: [Msg]) -> ioProperty $ do
+  model <- foldM (\ m msg -> transform msg m) initial messages
+  return $
+    counterexample (show model) $
+    cont model
+
+instance Arbitrary Msg where
+  arbitrary = oneof $
+    (Server <$> arbitrary) :
+    (Ui <$> arbitrary) :
+--    (Debug <$> arbitrary) :
+    []
+
+instance Arbitrary Server where
+  arbitrary = oneof $
+    (Update <$> arbitraryDoc) :
+--    pure Sync :
+    []
+
+instance Arbitrary Ui where
+  arbitrary = oneof $
+    (Enter <$> arbitrary) :
+    pure UpArrow :
+    pure DownArrow :
+    []
+
+arbitraryDoc :: Gen Document
+arbitraryDoc = do
+  edits :: [(Client, [Text])] <- arbitrary
+  return $ foldl' (\ old (client, new) -> mkPatch client old new) mempty edits
+
+instance Arbitrary Client where
+  arbitrary = Client <$> arbitrary
